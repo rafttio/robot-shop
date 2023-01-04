ca68beea74e59e8a2462c4b7baf41dc41650124b
Roy Iarchy <roy.iarchy@gmail.com>
2023-01-04 19:50:57 +0200

    Improvements

--- scripts/get-robot-web-address.sh
+++ scripts/get-robot-web-address.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Getting frontend service address"
+echo "Getting frontend ingress address"
 NAMESPACE=$1
 
 until [ ! -z $frontend ]; do
   sleep 1
-  export frontend=$(kubectl get service web -n $NAMESPACE -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
+  export frontend=$(kubectl get ingress web -n $NAMESPACE -o jsonpath='{.spec.rules[0].host}')
 done
 
 echo $frontend
